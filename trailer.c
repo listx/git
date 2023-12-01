5c7c7daf5f309644007727f5a176e063829ac2a2
Linus Arver <linusa@google.com>
2023-12-01 15:57:08 -0800

    sequencer: use the trailer iterator
    This patch allows for the removal of "trailer_info_get()" from the
    trailer.h API, which will be in the next patch.
    
    Instead of calling "trailer_info_get()", which is a low-level function
    in the trailers implementation (trailer.c), call
    trailer_iterator_advance(), which was specifically designed for public
    consumption in f0939a0eb1 (trailer: add interface for iterating over
    commit trailers, 2020-09-27).
    
    Avoiding "trailer_info_get()" means we don't have to worry about options
    like "no_divider" (relevant for parsing trailers). We also don't have to
    check for things like "info.trailer_start == info.trailer_end" to see
    whether there were any trailers (instead we can just check to see
    whether the iterator advanced at all).
    
    Also, teach the iterator about non-trailer lines, by adding a new field
    called "raw" to hold both trailer and non-trailer lines. This is
    necessary because a "trailer block" is a list of trailer lines of at
    least 25% trailers (see 146245063e (trailer: allow non-trailers in
    trailer block, 2016-10-21)), such that it may hold non-trailer lines.
    
    Signed-off-by: Linus Arver <linusa@google.com>

--- trailer.c
+++ trailer.c
@@ -1149,18 +1149,16 @@ void trailer_iterator_init(struct trailer_iterator *iter, const char *msg)
 
 int trailer_iterator_advance(struct trailer_iterator *iter)
 {
-	while (iter->internal.cur < iter->internal.info.trailer_nr) {
-		char *trailer = iter->internal.info.trailers[iter->internal.cur++];
-		int separator_pos = find_separator(trailer, separators);
+	if (iter->internal.cur < iter->internal.info.trailer_nr) {
+		char *line = iter->internal.info.trailers[iter->internal.cur++];
+		int separator_pos = find_separator(line, separators);
+		iter->is_trailer = (separator_pos > 0);
 
-		if (separator_pos < 1)
-			continue; /* not a real trailer */
-
+		iter->raw = line;
 		strbuf_reset(&iter->key);
 		strbuf_reset(&iter->val);
 		parse_trailer(&iter->key, &iter->val, NULL,
-			      trailer, separator_pos);
-		unfold_value(&iter->val);
+			      line, separator_pos);
 		return 1;
 	}
 	return 0;

b6dc8ddb5f80f93c87f1b2ec1fa86adfa5c78e09
Linus Arver <linusa@google.com>
2023-12-30 08:31:26 -0800

    trailer: template's final value does not depend on in_tok
    The populate_template_value() function takes an "in_tok" parameter to be
    the trailer (coming from the input text's trailer block) which happens
    to match the key of the template. Inside this function, the line
    
        arg = xstrdup(in_tok->value);
    
    which was originally introduced in 85039fb6e4 (trailer: execute command
    from 'trailer.<name>.command', 2014-10-13) is unnecessary. IOW
    populate_template_value() doesn't need the "in_tok" parameter at all.
    This makes sense because how we populate the value of the template never
    depends on anything we read from inside the trailer block. Indeed, we
    were already calling
    
        populate_template_value(template, NULL);
    
    with NULL for "in_tok" inside maybe_add_if_missing(). We could be
    passing in NULL for maybe_add_if_exists() as well, but at that point we
    would be always passing in NULL, so just drop it entirely.
    
    Signed-off-by: Linus Arver <linusa@google.com>

--- trailer.c
+++ trailer.c
@@ -255,24 +255,19 @@ static char *run_command_from_template(struct trailer_conf *conf,
  * Prepare the template by running the command (if any) requested by the
  * template in order to populate the template's value field.
  */
-static void populate_template_value(struct trailer_template *template,
-				    struct trailer_item *in_tok)
+static void populate_template_value(struct trailer_template *template)
 {
 	if (template->conf.command || template->conf.cmd) {
 		/*
 		 * Determine argument to pass into the command.
 		 */
 		const char *arg;
-		if (template->value && template->value[0]) {
+		if (template->value && template->value[0])
 			arg = template->value;
-		} else {
-			if (in_tok && in_tok->value)
-				arg = xstrdup(in_tok->value);
-			else
-				arg = xstrdup("");
-		}
-		template->value = run_command_from_template(&template->conf,
-							    arg);
+		else
+			arg = xstrdup("");
+
+		template->value = run_command_from_template(&template->conf, arg);
 		free((char *)arg);
 	}
 }
@@ -287,24 +282,24 @@ static void maybe_add_if_exists(struct trailer_template *template,
 		free_template(template);
 		break;
 	case EXISTS_REPLACE:
-		populate_template_value(template, in_tok);
+		populate_template_value(template);
 		apply_template_to_trailers(template, on_tok);
 		list_del(&in_tok->list);
 		free_trailer_item(in_tok);
 		break;
 	case EXISTS_ADD:
-		populate_template_value(template, in_tok);
+		populate_template_value(template);
 		apply_template_to_trailers(template, on_tok);
 		break;
 	case EXISTS_ADD_IF_DIFFERENT:
-		populate_template_value(template, in_tok);
+		populate_template_value(template);
 		if (check_if_different(template, in_tok, trailers, 1))
 			apply_template_to_trailers(template, on_tok);
 		else
 			free_template(template);
 		break;
 	case EXISTS_ADD_IF_DIFFERENT_NEIGHBOR:
-		populate_template_value(template, in_tok);
+		populate_template_value(template);
 		if (check_if_different(template, on_tok, trailers, 0))
 			apply_template_to_trailers(template, on_tok);
 		else
@@ -328,7 +323,7 @@ static void maybe_add_if_missing(struct trailer_template *template,
 		break;
 	case MISSING_ADD:
 		where = template->conf.where;
-		populate_template_value(template, NULL);
+		populate_template_value(template);
 		to_add = trailer_from(template);
 		if (after_or_end(where))
 			list_add_tail(&to_add->list, trailers);
